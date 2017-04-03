5e9ece78ebe649f01c58c65fba2ddc01e97bedc7
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-04-03 10:26:48 -0700

    base files

---
+++ SimpleCalc/SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by studentuser on 4/3/17.
+//  Copyright © 2017 Ethan.McGregor. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
